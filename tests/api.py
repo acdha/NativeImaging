a735998996d250acfa1ad47fb04ec060f062bcad
Chris Adams <chris@improbable.org>
2011-01-18 15:10:32 -0500

    Test cleanup & API expansion
    * added open_sample_image convenience method
    * Added tests for new image, crop & rotate based on PIL doctests

--- tests/api.py
+++ tests/api.py
@@ -30,19 +30,16 @@ class ApiConformanceTests(object):
         img = self.IMAGE_CLASS.open(StringIO(file(sample_jpg).read()))
         self.assertTrue(img)
 
-    def test_size(self):
+    def open_sample_image(self):
+        """Convenience method to return an open Image"""
         sample_jpg = os.path.join(SAMPLE_DIR, "5071384885_c5f331d337_b.jpg")
+        return self.IMAGE_CLASS.open(sample_jpg)
 
-        # Accepts filenames:
-        img = self.IMAGE_CLASS.open(sample_jpg)
-        self.assertEqual(img.size, (1024L, 680L))
+    def test_size(self):
+        self.assertEqual(self.open_sample_image().size, (1024L, 680L))
 
     def test_resize(self):
-        sample_jpg = os.path.join(SAMPLE_DIR, "5071384885_c5f331d337_b.jpg")
-
-        # Accepts filenames:
-        img = self.IMAGE_CLASS.open(sample_jpg)
-
+        img = self.open_sample_image()
         small = img.resize((128, 256))
 
         self.assertNotEqual(img, small)
@@ -53,10 +50,7 @@ class ApiConformanceTests(object):
         self.assertEqual(small.size, (128, 256))
 
     def test_thumbnail(self):
-        sample_jpg = os.path.join(SAMPLE_DIR, "5071384885_c5f331d337_b.jpg")
-
-        # Accepts filenames:
-        img = self.IMAGE_CLASS.open(sample_jpg)
+        img = self.open_sample_image()
 
         # Thumbnail is an in-place modification:
         self.assertEqual(img.thumbnail((128, 256)), None)
@@ -65,3 +59,35 @@ class ApiConformanceTests(object):
         # resize the image shouldn't be stretched so only one of our provided
         # values will be the same:
         self.assertEqual(img.size, (128L, 85L))
+
+    def disabled_new_image(self):
+        # TODO: Implement Image.new and enable this test
+        # Converted from PIL doc tests:
+        def _info(img):
+            return img.format, img.mode, img.size
+
+        # monochrome
+        self.assertEqual(_info(self.IMAGE_CLASS.new("1", (128, 128))), (None, '1', (128, 128)))
+
+        # grayscale (luminance)
+        self.assertEqual(_info(self.IMAGE_CLASS.new("L", (128, 128))), (None, 'L', (128, 128)))
+
+        # palette
+        self.assertEqual(_info(self.IMAGE_CLASS.new("P", (128, 128))), (None, 'P', (128, 128)))
+
+        # truecolor
+        self.assertEqual( _info(self.IMAGE_CLASS.new("RGB", (128, 128))), (None, 'RGB', (128, 128)))
+
+        # 32-bit integer
+        self.assertEqual(_info(self.IMAGE_CLASS.new("I", (128, 128))), (None, 'I', (128, 128)))
+
+        # 32-bit floating point
+        self.assertEqual(_info(self.IMAGE_CLASS.new("F", (128, 128))), (None, 'F', (128, 128)))
+
+    def test_crop(self):
+        img = self.open_sample_image()
+        self.assertEqual(img.crop((32, 32, 96, 96)).size, (64, 64))
+
+    def test_rotate(self):
+        img = self.open_sample_image()
+        self.assertEqual(img.rotate(45).size, (1024L, 680L))
