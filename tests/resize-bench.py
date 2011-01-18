b24237f57b8b1ef82cc8a841194db7122e00ff34
Chris Adams <chris@improbable.org>
2011-01-18 12:13:08 -0500

    resize-bench: added exception handling
    This allows simple timing collection for formats which break PIL such as JPEG 2000.

--- tests/resize-bench.py
+++ tests/resize-bench.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-import os
-from time import time
 from collections import defaultdict
+from time import time
+import os
+import sys
 
 from PIL import Image as PILImage
 from NativeImaging.backends.GraphicsMagick import GraphicsMagickImage as MagickImage
@@ -20,28 +21,34 @@ print "Comparison images are saved in %s" % OUTPUT_DIR
 for filename in os.listdir(SAMPLE_DIR):
     basename = os.path.splitext(filename)[0]
 
-    # PIL Baseline:
-    start_time = time()
+    try:
+        # PIL Baseline:
+        start_time = time()
 
-    master = PILImage.open(os.path.join(SAMPLE_DIR, filename))
+        master = PILImage.open(os.path.join(SAMPLE_DIR, filename))
 
-    master.thumbnail((256, 256), PILImage.ANTIALIAS)
+        master.thumbnail((256, 256), PILImage.ANTIALIAS)
 
-    master.save(open(os.path.join(OUTPUT_DIR, "%s_PIL.jpg" % basename), "wb"), "JPEG")
+        master.save(open(os.path.join(OUTPUT_DIR, "%s_PIL.jpg" % basename), "wb"), "JPEG")
 
-    TIMES[filename]['PIL'] = time() - start_time
+        TIMES[filename]['PIL'] = time() - start_time
+    except StandardError, e:
+        print >>sys.stderr, "PIL: Unhandled exception processing %s: %s" % (filename, e)
 
-    # Graphics Magick:
-    start_time = time()
+    try:
+        # Graphics Magick:
+        start_time = time()
 
-    master = MagickImage.open(os.path.join(SAMPLE_DIR, filename))
+        master = MagickImage.open(os.path.join(SAMPLE_DIR, filename))
 
-    master.thumbnail((256, 256), MagickImage.ANTIALIAS)
+        master.thumbnail((256, 256), MagickImage.ANTIALIAS)
 
-    # master.save(open(os.path.join(OUTPUT_DIR, "%s_graphicsmagick.jpg" % basename), "wb"), "JPEG")
-    master.save(os.path.join(OUTPUT_DIR, "%s_graphicsmagick.jpg" % basename), "JPEG")
+        # master.save(open(os.path.join(OUTPUT_DIR, "%s_graphicsmagick.jpg" % basename), "wb"), "JPEG")
+        master.save(os.path.join(OUTPUT_DIR, "%s_graphicsmagick.jpg" % basename), "JPEG")
 
-    TIMES[filename]['GraphicsMagick'] = time() - start_time
+        TIMES[filename]['GraphicsMagick'] = time() - start_time
+    except StandardError, e:
+        print >>sys.stderr, "GraphicsMagick: Unhandled exception processing %s: %s" % (filename, e)
 
 print
 print "Results"
@@ -49,5 +56,5 @@ print
 
 for f_name, scores in TIMES.items():
     print "%s:" % f_name
-    for lib, time in scores.items():
-        print "\t%16s: %0.2f" % (lib, time)
+    for lib, elapsed in scores.items():
+        print "\t%16s: %0.2f" % (lib, elapsed)
