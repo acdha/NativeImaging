76e1c3437bd0d5511c53065c49dd56243308b564
Daniel Krech <eikeon@eikeon.com>
2011-01-25 11:51:01 -0500

    added aware_cext to get_image_class

--- NativeImaging/__init__.py
+++ NativeImaging/__init__.py
@@ -8,6 +8,9 @@ def get_image_class(backend):
     if backend == "aware":
         from backends.aware import AwareImage
         return AwareImage
+    if backend == "aware_cext":
+        from backends.aware_cext import AwareImage
+        return AwareImage
     elif backend.lower() == "graphicsmagick":
         from backends.GraphicsMagick import GraphicsMagickImage
         return GraphicsMagickImage
