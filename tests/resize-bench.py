648d35166338b4a3309fd6a80491f1f11228451f
Chris Adams <chris@improbable.org>
2012-05-03 18:34:13 -0400

    resize-bench cleanup
    * PEP-8
    * Tidying imports

--- tests/resize-bench.py
+++ tests/resize-bench.py
@@ -6,84 +6,99 @@ from optparse import OptionParser
 from time import time
 import logging
 import os
-import subprocess
 import sys
 import tempfile
 
 from NativeImaging import get_image_class
 
 
-parser = OptionParser()
-parser.add_option("-v", default=0, dest="verbosity", action="count")
+def main():
+    parser = OptionParser()
+    parser.add_option("-v", default=0, dest="verbosity", action="count")
+    parser.add_option('--sample-dir',
+                      default=os.path.join(os.path.dirname(__file__),
+                                           "samples"),
+                      help="Path to test images (default: %default)")
+    parser.add_option('--output-dir',
+                      default=os.path.join(tempfile.gettempdir(),
+                                           "resize-bench"),
+                      help="Path to store output images (default:  %default)")
 
-(options, backend_names) = parser.parse_args()
+    (options, backend_names) = parser.parse_args()
 
-if options.verbosity > 1:
-    log_level = logging.DEBUG
-elif options.verbosity > 0:
-    log_level = logging.INFO
-else:
-    log_level = logging.WARNING
+    if options.verbosity > 1:
+        log_level = logging.DEBUG
+    elif options.verbosity > 0:
+        log_level = logging.INFO
+    else:
+        log_level = logging.WARNING
 
-logging.basicConfig(format="%(asctime)s [%(levelname)s]: %(message)s",
-                    level=log_level)
+    logging.basicConfig(format="%(asctime)s [%(levelname)s]: %(message)s",
+                        level=log_level)
 
-test_backends = backend_names or ('PIL', 'GraphicsMagick', 'Aware','java')
-BACKENDS = {}
+    return run_benchmark(backend_names, sample_dir=options.sample_dir,
+                         output_dir=options.output_dir)
 
-for backend_name in test_backends:
-    try:
-        BACKENDS[backend_name] = get_image_class(backend_name)
-    except (ImportError, KeyError):
-        print >>sys.stderr, "Can't load %s backend" % backend_name
 
-SAMPLE_DIR = os.path.join(os.path.dirname(__file__), "samples")
-OUTPUT_DIR = os.path.join(tempfile.gettempdir(), "resize-bench")
+def run_benchmark(backend_names, sample_dir=None, output_dir=None):
+    if not backend_names:
+        backend_names = ('PIL', 'GraphicsMagick', 'Aware', 'java')
 
-TIMES = defaultdict(dict)
+    backends = {}
 
-if not os.path.exists(OUTPUT_DIR):
-    os.makedirs(OUTPUT_DIR)
-
-print "Comparison images are saved in %s" % OUTPUT_DIR
-
-for filename in os.listdir(SAMPLE_DIR):
-    if filename.startswith("."):
-        continue
-
-    basename = os.path.splitext(filename)[0]
-
-    for backend_name, backend_class in BACKENDS.items():
-        logging.info("Resizing %s using %s", filename, backend_name)
-        start_time = time()
-
+    for backend_name in backend_names:
         try:
-            master = backend_class.open(os.path.join(SAMPLE_DIR, filename))
+            backends[backend_name] = get_image_class(backend_name)
+        except (ImportError, KeyError):
+            print >> sys.stderr, "Can't load %s backend" % backend_name
 
-            master.thumbnail((256, 256), backend_class.ANTIALIAS)
+    times = defaultdict(dict)
 
-            output_file = os.path.join(OUTPUT_DIR,
-                                        "%s_%s.jpg" % (basename, backend_name))
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-            master.save(open(output_file, "wb"), "JPEG")
+    print "Comparison images are saved in %s" % output_dir
 
-            TIMES[filename][backend_name] = time() - start_time
+    for filename in os.listdir(sample_dir):
+        if filename.startswith("."):
+            continue
 
-        # This allows us to use a blanket except below which has the nice
-        # property of catching direct Exception subclasses and things like
-        # java.lang.Exception subclasses on Jython:
-        except SystemExit:
-            sys.exit(1)
-        except:
-            logging.exception("%s: exception processing %s", backend_name,
-                            filename)
+        basename = os.path.splitext(filename)[0]
 
-print
-print "Results"
-print
+        for backend_name, backend_class in backends.items():
+            logging.info("Resizing %s using %s", filename, backend_name)
+            start_time = time()
 
-for f_name, scores in TIMES.items():
-    print "%s:" % f_name
-    for lib, elapsed in scores.items():
-        print "\t%16s: %0.2f" % (lib, elapsed)
+            try:
+                master = backend_class.open(os.path.join(sample_dir, filename))
+
+                master.thumbnail((256, 256), backend_class.ANTIALIAS)
+
+                output_file = os.path.join(output_dir,
+                                           "%s_%s.jpg" % (basename, backend_name))
+
+                master.save(open(output_file, "wb"), "JPEG")
+
+                times[filename][backend_name] = time() - start_time
+
+            # This allows us to use a blanket except below which has the nice
+            # property of catching direct Exception subclasses and things like
+            # java.lang.Exception subclasses on Jython:
+            except SystemExit:
+                sys.exit(1)
+            except:
+                logging.exception("%s: exception processing %s", backend_name,
+                                  filename)
+
     print
+    print "Results"
+    print
+
+    for f_name, scores in times.items():
+        print "%s:" % f_name
+        for lib, elapsed in scores.items():
+            print "\t%16s: %0.2f" % (lib, elapsed)
+        print
+
+if __name__ == "__main__":
+    main()
