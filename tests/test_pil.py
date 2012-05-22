acc1993b841d47a676435ce5c9b067ba46e513b5
Chris Adams <chris@improbable.org>
2012-05-22 16:49:00 -0400

    Add PIL tests to avoid regression

---
+++ tests/test_pil.py
@@ -0,0 +1,12 @@
+import unittest
+
+from api import ApiConformanceTests
+
+from NativeImaging import get_image_class
+
+class PILTests(ApiConformanceTests, unittest.TestCase):
+    IMAGE_CLASS = get_image_class("PIL")
+
+if __name__ == "__main__":
+    unittest.main()
+
