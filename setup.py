c92b2fa38c8a72d3feb7a3319ee47b68248bbc90
Chris Adams <cadams@loc.gov>
2017-10-30 17:26:13 -0400

    Update setup.py to use setuptools_scm

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
+# encoding: utf-8
+from __future__ import absolute_import, division, print_function
+
 from setuptools import setup
 import os
 
 
-def read_file(fn):
-    return open(os.path.join(os.path.dirname(__file__), fn)).read()
+def read_file(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
+        return f.read()
 
 
 setup(
     author='Chris Adams',
     author_email='chris@improbable.org',
     name='NativeImaging',
-    version='0.0.7',
+    use_scm_version=True,
     url='http://github.com/acdha/NativeImaging/',
     license='http://www.opensource.org/licenses/mit-license.php',
-    packages=['NativeImaging', 'NativeImaging.backends'],
     description='PIL-like interface for system imaging libraries',
     long_description=read_file("README.rst"),
+    packages=['NativeImaging', 'NativeImaging.backends'],
+    test_suite='tests',
+    setup_requires=['setuptools_scm'],
 )
